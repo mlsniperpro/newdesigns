f8f7b2f595d17724f45f572feeaeb1f80f91b2f3
mlsniperpro <algopro.km@gmail.com>
2023-04-01 13:02:13 +0000

    Adding spanish to plan selection

--- components/PlanSelection.jsx
+++ components/PlanSelection.jsx
@@ -15,29 +15,32 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 function PlanSelection() {
+  const [language, setLanguage] = React.useState("english"); //Language can be english or spanish
   return (
     <>
       {/* This is an example component */}
       <div className="flex min-h-screen pt-[30px] px-[40px]">
         <div className="min-w-full">
           <p className="text-[#00153B] text-[20px] leading-[40px] font-semibold">
-            Select Subscription Plan
+            {language === "english" ? "Select Subscription Plan" : "Seleccionar Plan de Suscripción"}
           </p>
           <div>
             <p className="text-[#717F87] text-[15px] leading-[27px] font-medium">
-              Choose the plan that works for you. You can always change it
-              later.
+              {language === "english" ? "Choose the plan that works for you. You can always change it later." : "Elige el plan que mejor se adapte a ti. Siempre puedes cambiarlo más tarde."}
+              
             </p>
           </div>
           <div className="mt-[30px] inline-flex border border-[#E1E3E5] shadow-[0px 1px 2px #E1E3E5] divide-[#E1E3E5] divide-x rounded-[5px]">
             <button className="bg-white hover:bg-[#F6F6F7] hover:text-[#717F87] text-[#0E1823] leading-[16px] text-[13px] font-semibold font-bold py-[15px] px-[25px] rounded-l">
-              <Link href="/">Free</Link>
+              <Link href="/">
+                {language === "english" ? "Free" : "Gratis"}
+              </Link>
             </button>
             <button className="bg-white hover:bg-[#F6F6F7] hover:text-[#717F87] text-[#0E1823] text-[13px] leading-[16px] font-semibold font-bold py-[15px] px-[25px] rounded-r">
-              Monthly
+              {language === "english" ? "Monthly" : "Mensual"}
             </button>
             <button className="bg-white hover:bg-[#F6F6F7] hover:text-[#717F87] text-[#0E1823] text-[13px] leading-[16px] font-semibold font-bold py-[15px] px-[25px] rounded-r">
-              Annual
+              {language === "english" ? "Annual" : "Anual"}
             </button>
           </div>
           <div className="mt-[20px] grid grid-cols-3 gap-[20px]">
@@ -49,45 +52,47 @@ function PlanSelection() {
                 <div className="flex justify-end">
                   <div className="bg-[#F6F6F7] rounded-[20px] flex justify-center align-center px-[12px]">
                     <p className="text-[#00153B] text-[12px] leading-[28px] font-bold">
-                      Starter
+                      {language === "english" ? "Starter" : "Principiante"}
                     </p>
                   </div>
                 </div>
                 <div>
                   <p className="text-[#00153B] text-[19px] leading-[24px] font-bold">
-                    Free
+                    {language === "english" ? "Free" : "Gratis"}
                   </p>
                   <p className="text-[#00153B] text-[50px] leading-[63px] font-bold">
+                    {language === "english" ? "FREE": "GRATIS"}
                     FREE
                   </p>
                 </div>
                 <div>
                   <p className="text-[#717F87] text-[18px] leading-[28px] font-medium">
-                    Limited Usage
+                    {language === "english" ? "Limited Usage" : "Uso Limitado"}
                   </p>
                   <p className="text-[#717F87] text-[18px] leading-[28px] font-medium">
-                    No Priority Customer Support
+                    {language === "english" ? "No Priority Customer Support" : "Sin Soporte Prioritario al Cliente"}
                   </p>
                 </div>
               </div>
               <div className="pt-[25px] px-[25px] pb-[35px]">
                 <div>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    Interract with the tutor to learn copywriting
+                    {language === "english" ? "Interract with the tutor to learn copywriting" : "Interactúa con el tutor para aprender redacción publicitaria"}
+                    
                   </p>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    Select pre-made options to get copies faster
+                    {language === "english" ? "Select pre-made options to get copies faster" : "Selecciona opciones prehechas para obtener copias más rápido"}
                   </p>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    Give a text-based command of your idea and get a copy faster
+                    {language === "english" ? "Give a text-based command of your idea and get a copy faster" : "Da una orden basada en texto de tu idea y obtén una copia más rápido"}
                   </p>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    Get Copy Based on Keyword
+                    {language === "english" ? "Get Copy Based on Keyword" : "Obtener copia basada en palabra clave"}
                   </p>
                 </div>
                 <div className="mt-[25px]">
                   <button className="bg-[#006EF5] rounded-[5px] py-[15px] px-[25px] text-[#fff] text-[14px] leading-[17px] font-semibold">
-                    Select Plan
+                    {language === "english" ? "Select Plan" : "Seleccionar Plan"}
                   </button>
                 </div>
               </div>
@@ -100,13 +105,13 @@ function PlanSelection() {
                 <div className="flex justify-end">
                   <div className="bg-[#F6F6F7] rounded-[20px] flex justify-center align-center px-[12px]">
                     <p className="text-[#00153B] text-[12px] leading-[28px] font-bold">
-                      Most Popular
+                      {language === "english" ? "Most Popular" : "Más Popular"}
                     </p>
                   </div>
                 </div>
                 <div>
                   <p className="text-[#00153B] text-[19px] leading-[24px] font-bold">
-                    One Month
+                    {language === "english" ? "Monthly" : "Mensual"}
                   </p>
                   <p className="text-[#00153B] text-[50px] leading-[63px] font-bold">
                     $9
@@ -114,25 +119,26 @@ function PlanSelection() {
                 </div>
                 <div>
                   <p className="text-[#717F87] text-[18px] leading-[28px] font-medium">
-                    Unlimited Usage
+                    {language === "english" ? "Unlimited Usage" : "Uso Ilimitado"}
                   </p>
                   <p className="text-[#717F87] text-[18px] leading-[28px] font-medium">
-                    1 month activation
+                    {language==="english"? "1 month activation" : "Activación de 1 mes"}
                   </p>
                 </div>
               </div>
               <div className="pt-[25px] px-[25px] pb-[35px]">
                 <div>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    Generate Copy Based on Keyword
+                    {language === "english"? "Generate Copy Based on Keyword" : "Generar copia basada en palabra clave"}
                   </p>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    Genrate Copy Based on Text Prompt
+                    {language === "english"? "Generate Copy Based on Text Prompt" : "Generar copia basada en texto"}
                   </p>
                 </div>
                 <div className="mt-[25px]">
                   <button className="bg-[#006EF5] rounded-[5px] py-[15px] px-[25px] text-[#fff] text-[14px] leading-[17px] font-semibold">
-                    <Link href="/monthly">Upgrade Now</Link>
+                    <Link href="/monthly">
+                      {language === "english" ? "Select Plan" : "Seleccionar Plan"}</Link>
                   </button>
                 </div>
               </div>
@@ -145,13 +151,13 @@ function PlanSelection() {
                 <div className="flex justify-end">
                   <div className="bg-[#F6F6F7] rounded-[20px] flex justify-center align-center px-[12px]">
                     <p className="text-[#00153B] text-[12px] leading-[28px] font-bold">
-                      Best Value
+                      {language === "english" ? "Best Value" : "Mejor Valor"}
                     </p>
                   </div>
                 </div>
                 <div>
                   <p className="text-[#00153B] text-[19px] leading-[24px] font-bold">
-                    One Year
+                    {language === "english" ? "Yearly" : "Anual"}
                   </p>
                   <p className="text-[#00153B] text-[50px] leading-[63px] font-bold">
                     $75
@@ -159,29 +165,33 @@ function PlanSelection() {
                 </div>
                 <div>
                   <p className="text-[#717F87] text-[18px] leading-[28px] font-medium">
-                    Unlimited Usage
+                    {language === "english" ? "Unlimited Usage" : "Uso Ilimitado"}
+                 
                   </p>
                   <p className="text-[#717F87] text-[18px] leading-[28px] font-medium">
-                    12 months activation
+                    {language === "english" ? "12 months activation" : "Activación de 12 meses"}
                   </p>
                 </div>
               </div>
               <div className="pt-[25px] px-[25px] pb-[35px]">
                 <div>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    2 months of free access
+                    {language === "english"? "2 months of free access" : "2 meses de acceso gratuito"}
                   </p>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium"></p>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    Generate Copy Based on Keyword
+                    {language === "english"? "Generate Copy Based on Keyword" : "Generar copia basada en palabra clave"}
                   </p>
                   <p className="text-[#717F87] text-[14px] leading-[24px] font-medium">
-                    Genrate Copy Based on Text Prompt
+                    {language === "english"? "Generate Copy Based on Text Prompt" : "Generar copia basada en texto"}
                   </p>
+
                 </div>
                 <div className="mt-[25px]">
                   <button className="bg-[#006EF5] rounded-[5px] py-[15px] px-[25px] text-[#fff] text-[14px] leading-[17px] font-semibold">
-                    <Link href="/yearly">Upgrade +</Link>
+                    <Link href="/yearly">
+                      {language === "english"? "Upgrade +": }
+                      Upgrade +</Link>
                   </button>
                 </div>
               </div>
