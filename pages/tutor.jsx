42fcecabe55c19253d82cd0f6a8a1fdffdeab9d3
mlsniperpro <algopro.km@gmail.com>
2023-04-05 15:23:52 +0000

    adding free tier to 10000

--- pages/tutor.jsx
+++ pages/tutor.jsx
@@ -55,7 +55,7 @@ function Tutor() {
     if (Date.now() < latest_subscription.subscriptionEndDate) {
       setSubScribed("subscribed");
       return;
-    } else if (currentUserWords[0]?.count < 3000) {
+    } else if (currentUserWords[0]?.count < 10000) {
       setSubScribed("subscribed");
       return;
     } else if (!currentUserWords[0]?.count) {
