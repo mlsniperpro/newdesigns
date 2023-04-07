acedbd4699aa38b3a8e38e3a33d12860fa1974cc
mlsniperpro <algopro.km@gmail.com>
2023-04-07 20:21:33 +0000

    adding db

--- pages/users.jsx
+++ pages/users.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../firebase";
+
 
 function Users() {
     const data = [
