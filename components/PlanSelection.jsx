dbed1a721a746863e5637cef54d0087e27cba1d1
Peter Okoth <peterokoth.km@gmail.com>
2023-04-11 17:51:03 +0300

    Make spanish default in selection

--- components/PlanSelection.jsx
+++ components/PlanSelection.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
 function PlanSelection() {
-  const [language, setLanguage] = React.useState("english"); //Language can be english or spanish
+  const [language, setLanguage] = React.useState("spanish"); //Language can be english or spanish
   return (
     <>
       {/* This is an example component */}
