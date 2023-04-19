1c43a95b4c1e17ac48a8e705946ebc1c272f6754
MuhammadZaeemNAveedSheikh <FA17-BCS-070@ISBSTUDENT.COMSTAS.EDU.PK>
2023-04-19 17:58:14 +0500

    api

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_KEY: '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',
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
   },
 }
 
