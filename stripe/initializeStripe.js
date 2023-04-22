559a919a056e334643577eef9c55f43ead4fab4a
mlsniperpro <algopro.km@gmail.com>
2023-04-22 00:05:35 -0400

    removing restricted key on client

--- stripe/initializeStripe.js
+++ stripe/initializeStripe.js
@@ -5,7 +5,7 @@ let stripePromise = null;
 const initializeStripe = async () => {
   if (!stripePromise) {
     stripePromise = await loadStripe(
-      "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"
+      process.env.NEXT_PUBLIC_RK
     );
   }
   return stripePromise;
