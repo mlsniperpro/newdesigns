db718d2f076204cc74f3ce30f198d1ee94e30161
mlsniperpro <algopro.km@gmail.com>
2023-05-07 15:54:51 +0300

    Correcting errors

---
+++ pages/Terms.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Terms() {
+  return (
+    <div>
+      <h1>Hello-Terms and Conditions page</h1>
+    </div>
+  )
+}
+
+export default Terms
---
+++ pages/privacy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function privacy() {
+  return (
+    <div>
+      <h1>Hello-privacy</h1>
+    </div>
+  )
+}
+
+export default privacy
