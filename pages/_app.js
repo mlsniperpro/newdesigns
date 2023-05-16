2b6f178018e2a362d06b7c80d7f291841a6b12bc
mlsniperpro <algopro.km@gmail.com>
2023-05-16 23:56:38 +0300

    The error mesage

--- pages/_app.js
+++ pages/_app.js
@@ -2,29 +2,44 @@ import React, { useEffect, useState } from "react";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-// Initialize the query client outside of the component so it doesn't get recreated on every render
 const queryClient = new QueryClient();
 
 function App({ Component, pageProps }) {
   const [rewardfulReady, setRewardfulReady] = useState(false);
+  const [displayErrorMessage, setDisplayErrorMessage] = useState(false);
 
   useEffect(() => {
-    // 'rewardful' function should be added to the window object by the external script
     const handleRewardfulReady = () => {
       console.log("Rewardful Ready!");
       setRewardfulReady(true);
     };
 
-    // Check if the 'rewardful' function is available in the global scope
     if (typeof window.rewardful === "function") {
       window.rewardful("ready", handleRewardfulReady);
     } else {
       console.error("Rewardful function not found in the global scope.");
-      // handle error scenario here, e.g., show an error message to the user, retry loading the script, etc.
     }
+
+    // Set a timeout to display an error message after 5 seconds
+    const timeoutId = setTimeout(() => {
+      if (!rewardfulReady) {
+        console.log("Rewardful not ready after 5 seconds. Displaying error message.");
+        setDisplayErrorMessage(true);
+      }
+    }, 5000);
+
+    // Clear the timeout if the component unmounts before the timeout fires
+    return () => clearTimeout(timeoutId);
   }, []);
 
-  // Here we could potentially render something different or show an error message if Rewardful isn't ready
+  if (displayErrorMessage) {
+    return (
+      <div>
+        Your browser does not support the required script. Please switch to another browser or enable JavaScript execution.
+      </div>
+    );
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       {rewardfulReady ? <Component {...pageProps} /> : <div>Loading...</div>}
@@ -33,4 +48,3 @@ function App({ Component, pageProps }) {
 }
 
 export default App;
-
