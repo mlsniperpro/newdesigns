bb31f724dfed1f3b6aaaa510be370b88f276f958
mlsniperpro <algopro.km@gmail.com>
2023-08-03 11:33:47 +0300

    done pdf routing

--- pages/pdf.tsx
+++ pages/pdf.tsx
@@ -1,5 +1,14 @@
 // Import React components
+import { useEffect } from 'react';
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/router';
+
+
+
+import useSubscription  from '@/hooks/useSubscription';
+
+
 
 import { Chat } from '@/components/PDF/components/chat';
 import { Header } from '@/components/PDF/components/header';
@@ -10,6 +19,11 @@ import { fontMono, fontSans } from '@/components/PDF/lib/fonts';
 import { nanoid } from '@/components/PDF/lib/utils';
 import { cn } from '@/components/PDF/lib/utils';
 
+
+
+import { auth } from '@/config/firebase';
+
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
@@ -17,8 +31,16 @@ interface RootLayoutProps {
 // RootLayout component
 export default function RootLayout({ children }: RootLayoutProps) {
   // Generate a unique id
+  const router = useRouter();
   const id = nanoid();
-
+  const [user, userLoading] = useAuthState(auth);
+  const {subscribed, loading} = useSubscription(user);
+  useEffect(() => {
+    if (!subscribed && !loading && user) {
+      router.push('/');
+    }
+  }, [loading, userLoading]);
+  
   return (
     <>
       <Toaster />
