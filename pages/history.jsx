588d6853e73f1b242c130fba612e13240df1045d
mlsniperpro <algopro.km@gmail.com>
2023-09-02 20:39:21 +0300

    Can edit title

--- pages/history.jsx
+++ pages/history.jsx
@@ -1,34 +1,43 @@
-import { IconTrash } from '@tabler/icons-react';
+import { IconEdit, IconTrash } from '@tabler/icons-react';
 import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 
-
-
+// Import the Edit icon
 import { auth, db } from '@/config/firebase';
-import { collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
-
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
 
 function App() {
   const [chats, setChats] = useState([]);
   const [selectedChatId, setSelectedChatId] = useState(null);
   const [selectedChat, setSelectedChat] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
-   const [showEmbedded, setShowEmbedded] = useState(false);
+  const [showEmbedded, setShowEmbedded] = useState(false);
+  const [editingChatId, setEditingChatId] = useState(null); // New state to manage editing mode
+  const [newTitle, setNewTitle] = useState(''); // New state to manage the new title
 
-   useEffect(() => {
-     const fetchChats = async () => {
-       const userId = auth.currentUser.uid;
-       const q = query(collection(db, 'chat'), where('userId', '==', userId));
-       const querySnapshot = await getDocs(q);
-       const fetchedChats = [];
-       querySnapshot.forEach((doc) => {
-         fetchedChats.push({ id: doc.id, ...doc.data() });
-       });
-       setChats(fetchedChats);
-     };
+  useEffect(() => {
+    const fetchChats = async () => {
+      const userId = auth.currentUser.uid;
+      const q = query(collection(db, 'chat'), where('userId', '==', userId));
+      const querySnapshot = await getDocs(q);
+      const fetchedChats = [];
+      querySnapshot.forEach((doc) => {
+        fetchedChats.push({ id: doc.id, ...doc.data() });
+      });
+      setChats(fetchedChats);
+    };
 
-     fetchChats();
-   }, []);
+    fetchChats();
+  }, []);
 
   useEffect(() => {
     const fetchChat = async () => {
@@ -43,17 +52,35 @@ function App() {
     }
   }, [selectedChatId]);
 
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, 'chat', id));
+    setChats(chats.filter((chat) => chat.id !== id));
+  };
+
+  const handleTitleUpdate = async (id) => {
+    await updateDoc(doc(db, 'chat', id), {
+      title: newTitle,
+    });
+    setChats(
+      chats.map((chat) =>
+        chat.id === id ? { ...chat, title: newTitle } : chat,
+      ),
+    );
+    setEditingChatId(null);
+    setNewTitle('');
+  };
+
   let filteredChats = chats.filter((chat) =>
     chat.title.toLowerCase().includes(searchTerm.toLowerCase()),
   );
-   if (showEmbedded) {
-     filteredChats = filteredChats.filter((chat) => chat.embedded === true);
-   }
+  if (showEmbedded) {
+    filteredChats = filteredChats.filter((chat) => chat.embedded === true);
+  }
 
   return (
     <div className="flex">
       {/* Sidebar */}
-      <div className="w-1/4 bg-gray-800 text-gray-300 p-4">
+      <div className="w-1/4 bg-gray-800 text-gray-300 p-4 overflow-y-auto h-screen">
         <input
           type="text"
           placeholder="Search Chats"
@@ -62,13 +89,13 @@ function App() {
           className="p-2 rounded border mb-4"
         />
         <label className="ml-4 flex items-center">
-            <input
-              type="checkbox"
-              checked={showEmbedded}
-              onChange={() => setShowEmbedded(!showEmbedded)}
-            />
-            <span className="ml-2">Show Only Embedded</span>
-          </label>
+          <input
+            type="checkbox"
+            checked={showEmbedded}
+            onChange={() => setShowEmbedded(!showEmbedded)}
+          />
+          <span className="ml-2">Show Only Embedded</span>
+        </label>
         <ul className="mt-4">
           {filteredChats.map((chat) => (
             <li
@@ -77,39 +104,44 @@ function App() {
               className="cursor-pointer hover:bg-gray-700 p-2 rounded mb-2 flex justify-between items-center"
             >
               <div className="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-messages"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
-                  <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
-                </svg>
-                <span className="text-sm ml-2">{chat.title}</span>
+                {editingChatId === chat.id ? (
+                  <input
+                    type="text"
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                  />
+                ) : (
+                  <span className="text-sm ml-2">{chat.title}</span>
+                )}
               </div>
-              <button
-                className="text-red-500"
-                onClick={() => {
-                  /* Delete logic here */
-                }}
-              >
-                <IconTrash size={18} />
-              </button>
+              <div>
+                {editingChatId === chat.id ? (
+                  <button onClick={() => handleTitleUpdate(chat.id)}>
+                    Save
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      setEditingChatId(chat.id);
+                      setNewTitle(chat.title);
+                    }}
+                  >
+                    <IconEdit size={18} />
+                  </button>
+                )}
+                <button
+                  className="text-red-500 ml-2"
+                  onClick={() => handleDelete(chat.id)}
+                >
+                  <IconTrash size={18} />
+                </button>
+              </div>
             </li>
           ))}
         </ul>
       </div>
-
       {/* Main Chat Area */}
-      <div className="w-3/4 p-4">
+      <div className="w-3/4 p-4 overflow-y-auto h-screen">
         {selectedChat && (
           <div className="flex flex-col h-screen p-4 bg-gray-50 text-black">
             <div className="flex flex-col flex-grow overflow-y-auto">
@@ -128,20 +160,6 @@ function App() {
                 ))}
               </ul>
             </div>
-            <form
-              className={`flex items-center w-full border-t p-4 text-black bg-gray-200`}
-            >
-              <input
-                placeholder="Say something..."
-                className={`flex-grow p-2 border rounded-md bg-white text-black ml-2 mr-2`}
-              />
-              <button
-                type="submit"
-                className="ml-4 p-2 bg-blue-500 text-white rounded-md"
-              >
-                Send
-              </button>
-            </form>
           </div>
         )}
       </div>
