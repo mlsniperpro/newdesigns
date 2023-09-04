52a1dfc0feedfac5f40a4bb525bdba2c93a39954
mlsniperpro <algopro.km@gmail.com>
2023-09-05 01:21:53 +0300

    Exporting to excel

--- pages/pdfcustomers.jsx
+++ pages/pdfcustomers.jsx
@@ -1,40 +1,80 @@
 // 1. Import Dependencies
 import { useEffect, useState } from 'react';
+
+
+
 import Link from 'next/link';
 
 
 
 import { auth, db } from '@/config/firebase';
 import { collection, getDocs, query, where } from 'firebase/firestore';
+import * as XLSX from "xlsx";
 
 
 const TableComponent = () => {
-  // 3. Fetch Data
-  const [data, setData] = useState([]);
-  const userId = auth.currentUser.uid;
+    const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const userId = auth.currentUser.uid;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const q = query(
-        collection(db, 'chat'),
-        where('userId', '==', userId),
-        where('embedded', '==', true),
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const q = query(
+          collection(db, 'chat'),
+          where('userId', '==', userId),
+          where('embedded', '==', true),
+        );
+        const querySnapshot = await getDocs(q);
+        const fetchedData = [];
+        querySnapshot.forEach((doc) => {
+          fetchedData.push(doc.data());
+        });
+        setData(fetchedData);
+        setFilteredData(fetchedData);
+      };
+      fetchData();
+    }, []);
+
+    const handleSearch = (e) => {
+      const term = e.target.value;
+      setSearchTerm(term);
+      if (term === '') {
+        setFilteredData(data);
+        return;
+      }
+      const filtered = data.filter(
+        (row) =>
+          row.name?.toLowerCase().includes(term.toLowerCase()) ||
+          row.email?.toLowerCase().includes(term.toLowerCase()) ||
+          row.phone?.toLowerCase().includes(term.toLowerCase()),
       );
-      const querySnapshot = await getDocs(q);
-      const fetchedData = [];
-      querySnapshot.forEach((doc) => {
-        fetchedData.push(doc.data());
-      });
-      setData(fetchedData);
+      setFilteredData(filtered);
     };
-    fetchData();
-  }, []);
 
-  return (
+    const exportToExcel = () => {
+      const ws = XLSX.utils.json_to_sheet(filteredData);
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Data');
+      XLSX.writeFile(wb, 'exported_data.xlsx');
+    };
+
+   return (
     <div className="container mx-auto p-4">
-      <Link href="/pdf" className="text-blue-500 hover:underline mb-4 inline-block">
-        PDF Home
+      <Link href="/" className="text-blue-500 hover:underline mb-4 inline-block">
+        Back Home
       </Link>
+      <input 
+        type="text" 
+        value={searchTerm} 
+        onChange={handleSearch} 
+        placeholder="Search..." 
+        className="mb-4 p-2 border rounded"
+      />
+      <button onClick={exportToExcel} className="mb-4 p-2 bg-blue-500 text-white rounded">
+        Export to Excel
+      </button>
       <table className="min-w-full bg-white rounded-lg overflow-hidden">
         <thead className="bg-gradient-to-r from-blue-400 to-blue-700 text-white">
           <tr>
@@ -44,11 +84,8 @@ const TableComponent = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((row, index) => (
-            <tr
-              key={index}
-              className={index % 2 === 0 ? 'bg-blue-100' : 'bg-blue-200'}
-            >
+          {filteredData.map((row, index) => (
+            <tr key={index} className={index % 2 === 0 ? 'bg-blue-100' : 'bg-blue-200'}>
               <td className="py-2 px-4 border">{row.name ?? 'N/A'}</td>
               <td className="py-2 px-4 border">{row.email ?? 'N/A'}</td>
               <td className="py-2 px-4 border">{row.phone ?? 'N/A'}</td>
@@ -59,6 +96,7 @@ const TableComponent = () => {
     </div>
   );
 };
+
 
 
 export default TableComponent;
