ace5832456d9984dfce6b4eb4b45ea26b7604b2c
mlsniperpro <algopro.km@gmail.com>
2023-09-06 01:29:58 +0300

    Embedded optimized

--- pages/embed.js
+++ pages/embed.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useReducer } from 'react';
 import toast from 'react-hot-toast';
 import ReactMarkdown from 'react-markdown';
 
@@ -7,66 +7,87 @@ import { useRouter } from 'next/router';
 
 import { auth } from '@/config/firebase';
 
-const CodeSnippetComponent = ({ theme = 'light' }) => {
-  const router = useRouter();
-  const { fileName } = router.query;
-
-  // State management for selected fields and text input fields
-  const [selectedFields, setSelectedFields] = useState({
-    name: false,
-    email: false,
-    phone: false,
-  });
-  const [textInputFields, setTextInputFields] = useState({
+const initialState = {
+  selectedFields: {},
+  textInputFields: {
     firstMessage: '',
     inputPlaceholder: '',
     chatName: '',
-  });
+  },
+};
 
-  // Theme and text color setup
-  const isDarkTheme = theme === 'dark';
-  const textColorClass = isDarkTheme ? 'text-white' : 'text-black';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_SELECTED_FIELD':
+      const currentSelectedFields = { ...state.selectedFields };
+      if (currentSelectedFields[action.payload.name]) {
+        delete currentSelectedFields[action.payload.name];
+      } else {
+        currentSelectedFields[action.payload.name] = true;
+      }
+      return {
+        ...state,
+        selectedFields: currentSelectedFields,
+      };
+    case 'SET_TEXT_INPUT_FIELD':
+      return {
+        ...state,
+        textInputFields: {
+          ...state.textInputFields,
+          [action.payload.name]: action.payload.value,
+        },
+      };
+    default:
+      return state;
+  }
+};
 
-  // Code snippet generation
-  const vionikoaiChat = {
-    userId: auth.currentUser.uid,
-    fileName,
-    ...selectedFields,
-    ...Object.fromEntries(
-      Object.entries(textInputFields).filter(([_, value]) => value),
-    ),
+const CodeSnippetComponent = ({ theme = 'light' }) => {
+  const router = useRouter();
+  const { fileName } = router.query;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const isDarkTheme = useMemo(() => theme === 'dark', [theme]);
+  const textColorClass = useMemo(
+    () => (isDarkTheme ? 'text-white' : 'text-black'),
+    [isDarkTheme],
+  );
+
+  const generateCodeSnippet = useMemo(() => {
+    const vionikoaiChat = {
+      userId: auth.currentUser.uid,
+      fileName,
+      ...state.selectedFields,
+      ...Object.fromEntries(
+        Object.entries(state.textInputFields).filter(([_, value]) => value),
+      ),
+    };
+    return `<script type="application/javascript">window.vionikoaiChat = ${JSON.stringify(
+      vionikoaiChat,
+      null,
+      2,
+    )};(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s);js.id = id;js.async = true;js.src = "https://mlsniperpro.github.io/vionikoaichatbox/client/chatWidget.js";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'vionikoaiChat-jssdk'));</script>`;
+  }, [state, fileName]);
+
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+    const payload = { name, value };
+    dispatch({
+      type:
+        type === 'checkbox' ? 'TOGGLE_SELECTED_FIELD' : 'SET_TEXT_INPUT_FIELD',
+      payload,
+    });
   };
-  const codeSnippet = `window.vionikoaiChat = ${JSON.stringify(
-    vionikoaiChat,
-    null,
-    2,
-  )}`;
-  const fullCodeSnippet = `<script>\n  ${codeSnippet}\n</script>\n<script src="https://mlsniperpro.github.io/vionikoaichatbox/client/chatWidget.js"></script>`;
 
-  // Markdown snippet for display
-  const markdownSnippet = `\`\`\`javascript\n${fullCodeSnippet}\n\`\`\``;
-
-  // Clipboard operations
   const handleCopyToClipboard = async () => {
     try {
-      await navigator.clipboard.writeText(fullCodeSnippet);
+      await navigator.clipboard.writeText(generateCodeSnippet);
       toast.success('Code copied to clipboard');
     } catch (err) {
       toast.error('Failed to copy code to clipboard');
     }
   };
 
-  // Checkbox and text input handling
-  const handleCheckboxChange = (e) => {
-    const { name, checked } = e.target;
-    setSelectedFields((prev) => ({ ...prev, [name]: checked }));
-  };
-  const handleTextInputChange = (e) => {
-    const { name, value } = e.target;
-    setTextInputFields((prev) => ({ ...prev, [name]: value }));
-  };
-
-  // UI rendering
   if (!fileName) {
     return <div>Loading...</div>;
   }
@@ -74,12 +95,11 @@ const CodeSnippetComponent = ({ theme = 'light' }) => {
   return (
     <div
       className={`flex flex-col justify-center items-center h-screen ${textColorClass}`}
-      style={{ background: 'linear-gradient(to right, #1e3c72, #2a5298)' }}
     >
       <div
         className={`prose prose-sm ${textColorClass} p-4 rounded-lg shadow-md bg-white`}
       >
-        <ReactMarkdown>{markdownSnippet}</ReactMarkdown>
+        <ReactMarkdown>{`\`\`\`javascript\n${generateCodeSnippet}\n\`\`\``}</ReactMarkdown>
         <button
           onClick={handleCopyToClipboard}
           className="mt-4 p-2 bg-blue-500 text-white rounded-md"
@@ -94,8 +114,8 @@ const CodeSnippetComponent = ({ theme = 'light' }) => {
                 <input
                   type="checkbox"
                   name={field}
-                  checked={selectedFields[field]}
-                  onChange={handleCheckboxChange}
+                  checked={state.selectedFields[field]}
+                  onChange={handleInputChange}
                   className="mr-2"
                 />
                 {field.charAt(0).toUpperCase() + field.slice(1)}
@@ -113,8 +133,8 @@ const CodeSnippetComponent = ({ theme = 'light' }) => {
                 <input
                   type="text"
                   name={field}
-                  value={textInputFields[field]}
-                  onChange={handleTextInputChange}
+                  value={state.textInputFields[field]}
+                  onChange={handleInputChange}
                   className="p-2 border rounded-md"
                 />
               </label>
