edaf2ce0c4d46aa342c908cbc3b16043efe0552f
mlsniperpro <algopro.km@gmail.com>
2023-09-18 21:32:07 +0300

    OPTIMIZING

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+
 import useSubscription from '@/hooks/useSubscription';
+
 import Dashboard from '../components/dashboard';
-import Loader from '@/components/Loader';
 import PlanSelection from '@/components/PlanSelection';
+
 import { auth } from '../config/firebase';
+
 import { parseCookies } from 'nookies';
+import PropTypes from 'prop-types';
+
+const MainContent = React.memo(
+  ({ loading, subscriptionDetails, upgrade, onValueChange }) => {
+    if (loading) {
+      return <Dashboard />;
+    }
+
+    if (
+      (subscriptionDetails.payPalStatus === 'ACTIVE' ||
+        subscriptionDetails.subscribed ||
+        subscriptionDetails.fairUse) &&
+      !upgrade
+    ) {
+      return (
+        <Dashboard
+          onValueChange={onValueChange}
+          subscriptionDetails={subscriptionDetails}
+        />
+      );
+    }
+
+    return <PlanSelection />;
+  },
+);
+
+MainContent.displayName = 'MainContent';
+
+MainContent.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  subscriptionDetails: PropTypes.object.isRequired,
+  upgrade: PropTypes.bool.isRequired,
+  onValueChange: PropTypes.func.isRequired,
+};
+
 function Index() {
   const [user, loadingAuth] = useAuthState(auth);
   const [upgrade, setUpgrade] = useState(false);
-  const { loading, subscriptionDetails } = useSubscription(user); // use custom hook
+  const { loading, subscriptionDetails } = useSubscription(user);
   const router = useRouter();
-  const handleValueChange = (newValue) => {
+
+  const handleValueChange = useCallback((newValue) => {
     setUpgrade(newValue);
-  };
+  }, []);
+
   useEffect(() => {
     if (
       !auth?.currentUser?.uid &&
@@ -25,33 +66,36 @@ function Index() {
       router.replace('/home');
     }
   }, [user, loadingAuth]);
+
+  const mainContentProps = useMemo(
+    () => ({
+      loading,
+      subscriptionDetails,
+      upgrade,
+      onValueChange: handleValueChange,
+    }),
+    [loading, subscriptionDetails, upgrade, handleValueChange],
+  );
+
   return (
     <div>
       <Head>
         <title>Vionko Marketing AI</title>
       </Head>
-      {console.log("The loading state is ",loading)}
-      {console.log("The subscription details are ",subscriptionDetails)}
-      {console.log("The userid is ",user?.uid)}
       <div className="bg-[#1A232E] flex flex-col items-center justify-center min-h-screen py-2">
         <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-          {loading ? (
-            <Dashboard />
-          ) : (subscriptionDetails.payPalStatus=="ACTIVE"||subscriptionDetails.subscribed || subscriptionDetails.fairUse) && !upgrade ? (
-            <Dashboard onValueChange={handleValueChange} subscriptionDetails={subscriptionDetails} />
-          ) : (
-            <PlanSelection />
-          )}
+          <MainContent {...mainContentProps} />
         </main>
       </div>
     </div>
   );
 }
+
 export default Index;
+
 export async function getServerSideProps(context) {
   const cookies = parseCookies(context);
 
-  // Check if the user is authenticated
   if (!cookies.auth) {
     return {
       redirect: {
@@ -61,7 +105,6 @@ export async function getServerSideProps(context) {
     };
   }
 
-  // If the user is authenticated, return the default props
   return {
     props: {},
   };
