9493a48bc8600e61ee6cee48195f14a547dedb1f
mlsniperpro <algopro.km@gmail.com>
2023-12-26 02:58:56 +0300

    Pass email and passwords well

--- src/app/components/AuthInput.jsx
+++ src/app/components/AuthInput.jsx
@@ -1,7 +1,12 @@
-const AuthInput = ({placeholder, type}) => {
+const AuthInput = ({ onChange, placeholder, type }) => {
   return (
-    <input placeholder={placeholder} type={type} className="h-20 bg-neutral-50 border-b border-Claro1 w-full [&::placeholder]:opacity-50 text-gray-700 text-2xl font-['Lato'] leading-loose outline-none focus:border-b-Coporativo1 transition-all" />
-  )
-}
+    <input
+      placeholder={placeholder}
+      type={type}
+      onChange={onChange} // Add this line
+      className="h-20 bg-neutral-50 border-b border-Claro1 w-full [&::placeholder]:opacity-50 text-gray-700 text-2xl font-['Lato'] leading-loose outline-none focus:border-b-Coporativo1 transition-all"
+    />
+  );
+};
 
-export default AuthInput
+export default AuthInput;
